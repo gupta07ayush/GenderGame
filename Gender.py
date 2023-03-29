39ecfcec9f2ccda7a693100f315768208e5475c5
Ayush Gupta <gupta07ayush@gmail.com>
2023-03-29 20:07:32 +0530

    added country input field" s dfef

--- Gender.py
+++ Gender.py
@@ -11,8 +11,9 @@ def predict_gender():
     try:
         # getting the input from entry
         entered_name = name_entry.get()
+        entered_country=country_entry.get()
         # making a request to the API, the user's entered name is injected in the url
-        response = requests.get(f'https://api.genderize.io/?name={entered_name}').json()
+        response = requests.get(f'https://api.genderize.io/?name={entered_name}&country_id={entered_country}').json()
         # getting name from the response
         name = response['name']
         # getting gender from the response  
@@ -32,8 +33,8 @@ def predict_gender():
 
 
 #colors for the Application
-gold = '#dca714'
-brown = '#31251d'
+gold = '#ffffff'
+brown = '#000000'
 
 #creating the main window
 window = Tk()
@@ -48,7 +49,7 @@ window.resizable(height=FALSE, width=FALSE)
 top_frame = Frame(window, bg=brown, width=325, height=80)
 top_frame.grid(row=0, column=0)
 
-bottom_frame = Frame(window, bg="pink", width=300, height=250)
+bottom_frame = Frame(window, bg="#000080", width=300, height=250)
 bottom_frame.grid(row=1, column=0)
 
 #inside top frame, create two labels
@@ -61,29 +62,38 @@ second_label.grid(row=1, column=0)
 #widgets inside the bottom_frame
 #the name label
 label = Label(bottom_frame, text="Name: ", font=('Poppins 10 bold'), justify=LEFT)
-label.place(x=4, y=10)
+label.place(x=4, y=5)
 
 #input field for name label
 name_entry = Entry(bottom_frame, width=25, font=("Poppins 15 bold"))
-name_entry.place(x=5, y=35)
+name_entry.place(x=4, y=30)
+
 
 #Empty place for name label, to display name
-name_label = Label(bottom_frame, text="")
+name_label = Label(bottom_frame, text="",bg="#000080")
 name_label.place(x=5, y=70)
 
 #Empty gender name label, it will be used to display the name
-gender_label = Label(bottom_frame, text='', font=('Poppins 10 bold'))
+gender_label = Label(bottom_frame, text='',bg="#000080", font=('Poppins 10 bold'))
 gender_label.place(x=5, y=90)
 
 # the empty probability label, it will be used to display the gender probalility
-probability_label = Label(bottom_frame, text='', font=('Poppins 10 bold'))
+probability_label = Label(bottom_frame, text='', bg="#000080", font=('Poppins 10 bold'))
 probability_label.place(x=5, y=110)
+
+
 
 #the predict button
 predict_button = Button(bottom_frame, text="PREDICT", bg=gold, fg=brown, font=('Poppins 10 bold'), command=predict_gender)
 predict_button.place(x=10,y=140)
 
+#the country label
+label2 = Label(bottom_frame, text="Country: ", font=('Poppins 10 bold'), justify=LEFT)
+label2.place(x=4, y=170)
 
+#country field for name label
+country_entry = Entry(bottom_frame, width=25, font=("Poppins 15 bold"))
+country_entry.place(x=4, y=190)
 
 
 
